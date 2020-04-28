2f79a99dccee15a22b8eb2e783604675deff7eff
Anastashionm <63411565+Anastashionm@users.noreply.github.com>
2020-04-28 19:36:01 +0400

    Add files via upload

---
+++ Лаб.№1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, P;
+            Console.Write("Введите количество секунд n:");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Введите пройденный путь P:");
+            P = int.Parse(Console.ReadLine());
+            double s=0, sr=0;
+            for (int i = 0; i <n; i++)
+                s += P*Math.Sqrt(i+1)+i;//общий пройденный путь
+            sr = s / n;//пусть пройденный в среднем за 1 секунду
+            Console.Write("Путь, проходимый телом за одну секунду в среднем равен sr={0:.###} ", sr);
+            Console.ReadKey();
+        }
+    }
+}
